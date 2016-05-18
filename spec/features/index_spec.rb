234eeae41b9c0651e4dfa9d008f87a2d1454891f
Yan Zhang <emma2740@gmail.com>
2016-05-18 11:14:23 +0200

    Add images and tests for projects

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -4,14 +4,22 @@ describe 'index', type: :feature do
     visit '/'
   end
 
-  it 'says hello to Yan' do
+  it 'says my name' do
     expect(page).to have_selector 'h1'
     within 'h1' do
-      expect(page).to have_content 'hello yan'
+      expect(page).to have_content 'Yan Zhang'
     end
   end
 
-  it 'display Middleman logo' do
-    expect(page).to have_css 'img[src*="middleman-logo.svg"]'
+  # it 'display logo' do
+  #   expect(page).to have_css 'img[src*="yan.jpg"]'
+  # end
+
+  it 'displays project list' do
+    expect(page).to have_css '.projects'
+    within '.projects' do
+      expect(page).to have_content 'My First Website'
+      expect(page).to have_content 'FizzBuzz'
+    end
   end
 end
