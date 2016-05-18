acdadef04d1e42c04f4ffd2e5c94b9e99a80c175
Yan Zhang <emma2740@gmail.com>
2016-05-18 15:18:43 +0200

    Add configuraton for deployment

--- config.rb
+++ config.rb
@@ -5,40 +5,22 @@
 # Per-page layout changes:
 #
 # With no layout
+require 'extensions/build_cleaner'
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.deploy_method = :git
+end
 
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
-# General configuration
+configure :build do
+  activate :relative_assets
+  activate :build_cleaner
+end
 
 # Reload the browser automatically whenever files change
 configure :development do
   activate :livereload
-end
-
-###
-# Helpers
-###
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-# Build-specific configuration
-configure :build do
-  # Minify CSS on build
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
 end
