b190524711d1aa28950f602dd612516bbe109c66
Yan Zhang <emma2740@gmail.com>
2016-05-19 12:14:38 +0200

    Add project info add data in projects.yml add display on index page

--- config.rb
+++ config.rb
@@ -6,6 +6,8 @@
 #
 # With no layout
 require 'extensions/build_cleaner'
+require 'date'
+
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
@@ -23,4 +25,17 @@ end
 # Reload the browser automatically whenever files change
 configure :development do
   activate :livereload
+end
+
+helpers do
+  def today
+    Date.today
+  end
+
+  def get_image(image_url)
+    if image_url.nil?
+      image_url = 'placeholder.jpg'
+    end
+    image_url
+  end
 end
